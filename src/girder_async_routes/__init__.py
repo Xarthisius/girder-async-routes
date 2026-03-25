39ede552f8edabcfa7a80c350dfa64d1889ea161
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2026-03-25 11:41:23 -0500

    Add autoversioning

--- src/girder_async_routes/__init__.py
+++ src/girder_async_routes/__init__.py
@@ -1,3 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
 from .routes import async_file_routes
 
-__all__ = ["async_file_routes"]
+try:
+    __version__ = version("girder-async-routes")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+__all__ = ["async_file_routes", "__version__"]
